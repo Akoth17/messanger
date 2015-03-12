09b163db03c946a3e19b9322214a989e6ea36e2f
Akoth17 <Akoth17@berkeley.edu>
2015-03-12 16:12:41 -0700

    Initial Commit

---
+++ messanger/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  messanger
+//
+//  Created by Andrew Koth on 3/12/15.
+//  Copyright (c) 2015 Andrew Koth. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
